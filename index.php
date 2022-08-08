ce3aec02d2b7ffa9e0cb15f8204094183a7df803
s-amuel-18 <63316336+s-amuel-18@users.noreply.github.com>
2022-08-08 10:47:35 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- font -->
+    <!-- end font -->
+
+    <!-- styles -->
+    <!-- end styles -->
+
+    <title>Fee Agency</title>
+</head>
+
+<body>
+    <nav>
+
+    </nav>
+
+    <main>
+
+    </main>
+
+    <!-- scripts -->
+    <!-- end scripts -->
+</body>
+
+</html>
