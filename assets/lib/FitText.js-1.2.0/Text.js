fdf0bfc2b8234ae7645f07e61ac3d052c2e2f002
branmarvel <88461465+branmarvel@users.noreply.github.com>
2022-08-08 17:15:32 -0400

    comit inicial pagina al 80%

---
+++ assets/lib/FitText.js-1.2.0/Text.js
@@ -0,0 +1,7 @@
+$(".fittext1").fitText(1.5, { minFontSize: "20px", maxFontSize: "50px" });
+$(".fittext2").fitText(1.2, { minFontSize: "20px", maxFontSize: "50px" });
+$(".fittext3").fitText(1.5, { minFontSize: "20px", maxFontSize: "50px" });
+$(".fittext4").fitText(1.2, { minFontSize: "20px", maxFontSize: "50px" });
+$(".fittext3").fitText(1.1, { minFontSize: "20px", maxFontSize: "100px" });
+
+// jQuery("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px' });
--- index.php
+++ index.php
@@ -4,12 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600&display=swap" rel="stylesheet">
     <!-- end font -->
 
     <!-- styles -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./assets/lib/bootstrap-4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/lib/intl-tel-input/css/intlTelInput.min.css">
     <!-- end styles -->
 
     <title>Fee Agency</title>
@@ -17,14 +24,340 @@
 
 <body>
     <nav>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <a class="navbar-brand " href="#"><img width="150" src="./assets/Logos/Recurso 8.png"
+                    alt="Fee agency logo"></a>
+            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse"
+                data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link text-uppercase px-4 text-primary" href="#home">HOME <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase px-4" href="#quienes_somos">QUIENES SOMOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase px-4" href="#servicios">SERVICIOS</a>
+                    </li>
 
+                    <li class="nav-item">
+                        <a class="nav-link px-4 text-uppercase" href="#contact">CONTACTO</a>
+                    </li>
+
+
+
+                </ul>
+            </div>
+        </nav>
     </nav>
 
     <main>
+        <section id="home">
+            <div class="container-fluid bg-img" style="background-image: url(./assets/images/BlankMap-World_gray.png);">
+                <div class="container">
+                    <div class="row d-flex align-items-center ">
+                        <div class="col-12 col-md-6 my-3">
+                            <div class="">
+                                <img class=" w-100" src="./assets/images/azafata.png" alt="">
+                                <!-- <img class="" src="./assets/images/Recurso 11.svg" alt="">
+                                <img class="" src="./assets/images/Recurso 21.svg" alt=""> -->
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 my-3 text-center text-md-left">
+                            <div class="my-3">
+                                <h1 class="title-1 fittext3">¿Quieres ser el ejecutivo de ventas que siempre soñastes?
+                                </h1>
+                            </div>
+                            <div class="my-3 fittext3">
+                                <h2 class="text-primary text-uppercase title-1">¡con nosotros puedes lograrlo !</h2>
+                            </div>
+                            <div class="d-flex flex-column flex-md-row  my-3 ">
+                                <div class=" my-2 "><button type="button"
+                                        class="btn btn-primary  px-5">Registro</button>
+                                </div>
+                                <div class=" my-2 mx-4"><button type="button" class="btn btn-secondary  px-5">Inicio
+                                        de
+                                        Sesión</button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container py-4">
+                <div class="d-flex flex-column align-items-center ">
+                    <h4 class="text-muted text-center"> Te ofrecemos la oportunidad de ser parte de nuestro negocio,
+                        <br>
+                        con
+                        actrativos
+                        beneficios y licencia gratuita.
+                    </h4>
+                    <div class=" py-3"><button type="button" class="btn btn-primary  px-5">Postulate
+                            aquí</button>
+                    </div>
+                </div>
 
+            </div>
+        </section>
+
+        <section class="" id="quienes_somos">
+            <div class="container">
+                <div class=" plus-icon-1 d-none d-md-block ml-auto mr-auto">
+                    <img src="./assets/images/plus-icon.svg" alt="">
+                </div>
+            </div>
+            <div class="bg-primary p-3" style="min-height:80vh ;">
+
+                <div class="container ">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <img class="d-none d-md-block" src="./assets/images/celular.png" alt="celular" width="80%">
+                        </div>
+                        <div class="col-12 col-md-6 d-flex flex-column justify-content-center text-white">
+                            <div class="">
+                                <h1 class="title-1 ">Quiénes somos</h1>
+                            </div>
+                            <div class="">
+                                <img class="d-md-none d-block float-left mr-3" src="./assets/images/celular.png"
+                                    alt="celular" width="100px">
+                                <p class=" text-justify">Somos Agency Fee, una empresa 100% Americana y estamos
+                                    conformados
+                                    por un equipo
+                                    altamente
+                                    especializado y en constante formacion que esta dispuesto siempre para apoyarte en
+                                    todo
+                                    lo
+                                    que necesites.</p>
+                            </div>
+                            <div class="d-flex">
+                                <div class="bg-white p-1 bar">
+
+                                </div>
+                                <div class="px-3">
+                                    <h6>Apoyamos a las agencias de viajes independientes en el área de consolidación
+                                    </h6>
+                                </div>
+                            </div>
+                            <div class="d-flex mt-3">
+                                <div class="bg-primary p-1 bar">
+
+                                </div>
+                                <div class="px-3">
+                                    <h6>
+                                        Ofrecemos la posibilidad de simplicar el trabajo de los Agentes de Viajes al
+                                        momento
+                                        de
+                                        efectuar
+                                        reservas y emisiones.
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="plus-icon-2 d-block d-md-none ml-auto mr-auto">
+                    <img src="./assets/images/plus-icon.svg" alt="">
+                </div>
+            </div>
+
+        </section>
+
+        <section id="servicios">
+            <div class="container wrapper serv-card">
+                <div class="row ">
+                    <div class="col-12 col-md-6 py-4  ">
+                        <div class="bg-white bar rounded">
+                            <div class="d-flex ">
+                                <div class="">
+                                    <img src="./assets/images/girl-small.png" width="100%" alt="">
+                                </div>
+                                <div class=" d-flex flex-column justify-content-center px-3 pt-3">
+                                    <div class="title-1">
+                                        <h4>Soy agente</h4>
+                                    </div>
+                                    <div class="text-muted text-uppercase">
+                                        <p>Y quiero empezar a asesorarme con agency fee</p>
+                                    </div>
+                                    <div class=" py-3"><button type="button" class="btn btn-primary  px-5">Más
+                                            información</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 py-4  ">
+                        <div class="bg-white bar rounded">
+                            <div class="d-flex ">
+                                <div class="">
+                                    <img src="./assets/images/girl-small.png" width="100%" alt="">
+                                </div>
+                                <div class=" d-flex flex-column justify-content-center px-3 pt-3">
+                                    <div class="title-1">
+                                        <h4>Soy agente</h4>
+                                    </div>
+                                    <div class="text-muted text-uppercase">
+                                        <p>Y quiero empezar a asesorarme con agency fee</p>
+                                    </div>
+                                    <div class=" py-3"><button type="button" class="btn btn-primary  px-5">Más
+                                            información</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="  text-center py-5">
+                    <h2 class="font-weight-bold fittext3">"Lograr la excelencia en la atención al cliente y en la <br>
+                        calidad del servicio
+                        que prestamos a su
+                        agencia de viajes, <br> <span class="text-primary text-uppercase">ES NUESTRO GRAN
+                            COMPROMISO</span>"
+                    </h2>
+                </div>
+            </div>
+
+
+        </section>
+
+        <section class="pb-5" id="formulario">
+            <div class="container  p-4">
+                <div class="text-center">
+                    <h1 class="font-weight-bold text-uppercase">Formulario</h1>
+                    <p class="text-muted"> Te ofrecemos la oportunidad de ser parte de nuestro negocio, con<span
+                            class="text-primary"> actractivos beneficios y licencia gratuita.</span>
+                    </p>
+                </div>
+
+                <form action="">
+
+                    <div class="form-group">
+
+                        <div class="row">
+                            <div class="col-6 col-md-4 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="Nombre">
+                            </div>
+                            <div class="col-6 col-md-4 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="Apellido">
+                            </div>
+                            <div class="col-12 col-md-4 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="Coreo Electrónico">
+                            </div>
+                            <div class="col-12 col-md-6 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="País de Residencia ">
+                            </div>
+                            <div class="col-12 col-md-6 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="N° de pasaporte">
+                            </div>
+                            <div class="col-12 col-md-6 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="Nacionalidad">
+                            </div>
+                            <div class="col-12 col-md-6 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="Estado">
+                            </div>
+                            <div class="col-12 col-md-6 py-3">
+                                <input type="text" class="form-control form-control-sm" name="" id=""
+                                    aria-describedby="" placeholder="Direccion">
+
+                            </div>
+                            <div class="col-12 col-md-6 py-3">
+                                <input type="tel" id="phone">
+                            </div>
+                        </div>
+                        <div class="col-4 offset-4 p-3 bg-primary">
+                            <input type="file" class="form-control-file d-inline" id="exampleFormControlFile1">
+                        </div>
+
+
+                    </div>
+                </form>
+            </div>
+
+
+            </div>
+
+        </section>
+
+        <Footer>
+            <div class="bg-primary d-none d-md-block">
+                <div class="container  d-flex text-white py-3">
+                    <img src="" alt="">
+                    <h5>Esta web utiliza cookies propias para facilitar la navegación y cookies de terceros para obtener
+                        estadísticas de uso y satisfacción.</h5>
+                    <div class=""><a name="" id="" class="btn btn-light text-secondary" href="#" role="button">
+                            Aceptar</a></div>
+                </div>
+            </div>
+
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-12 d-block d-md-none text-center py-3"><a class="" href="#home"><img width="200"
+                                src="./assets/Logos/Recurso 8.png" alt="Agency Fee logo"></a>
+                    </div>
+
+                    <div class="col-4 order-2 order-md-1 py-3">
+                        <a class="d-none d-md-block " href="#home"><img width="150" src="./assets/Logos/Recurso 8.png"
+                                alt="Agency Fee logo"></a>
+                        <p class="text-muted pt-5 pt-md-1 ">All Reserved Desing 2020 </p>
+                    </div>
+
+                    <div class="col-4 order-1 order-md-2">
+                        <h6 class="font-weight-bold py-3">Contact</h6>
+                        <p class="text-muted ">info@agencyfree.com</p>
+                    </div>
+
+                    <div class="col- order-3">
+                        <h6 class="font-weight-bold py-3">Social Media</h6>
+                        <div class="d-flex">
+                            <div class=""></div>
+                            <div class=""></div>
+                            <div class=""></div>
+                            <div class=""></div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </Footer>
     </main>
 
     <!-- scripts -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="./assets/lib/jquery/jquery-3.6.0.min.js"></script>
+    <script src="./assets/lib/bootstrap-4/js/bootstrap.bundle.min.js"></script>
+    <!-- intl-tel-input -->
+    <!-- <input type="tel" id="phone"> -->
+    <script src="./assets/lib/intl-tel-input/js/intlTelInput.min.js"></script>
+    <script src="./assets/lib/FitText.js-1.2.0/jquery.fittext.js"></script>
+    <script type="text/javascript">
+    $(".fittext1").fitText();
+    $(".fittext2").fitText(1.5);
+    $(".fittext3").fitText(2.2, {
+        minFontSize: '15px',
+        maxFontSize: '28px'
+    });
+    </script>
+    <script>
+    var input = document.querySelector("#phone");
+    window.intlTelInput(input, {
+        // any initialisation options go here
+    });
+    </script>
     <!-- end scripts -->
 </body>
 
