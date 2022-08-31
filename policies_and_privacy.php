6ad0f5c141574552e087b2c695429d1f2349d204
s-amuel-18 <63316336+s-amuel-18@users.noreply.github.com>
2022-08-31 16:31:06 -0400

    se realizó la pagina de políticas y privacidad

--- policies_and_privacy.php
+++ policies_and_privacy.php
@@ -9,7 +9,44 @@
             </div>
 
             <!-- aqui va la descripcion -->
-            <div class="">
+            <div class="container">
+
+                <h2>Política de Privacidad</h2>
+
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have
+                    suffered alteration in some form, by injected humour, or randomised words which don't
+                    look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need
+                    to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem
+                    lpsum generators on the Internet tend to repeat predefined chunks as necessary, making
+                    this the first true generator on the Internet. It uses a dictionary of over 200 Latin words,
+                    combined with a handful of model sentence structures, to generate Lorem Ipsum which
+                    looks reasonable. The generated Lorem Ipsum is therefore always free from repetition,
+                    injected humour, or non-characteristic words etc.
+                </p>
+
+
+                <h2>Política de Privacidad 2</h2>
+
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have
+                    suffered alteration in some form, by injected humour, or randomised words which don't
+                    look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need
+                    to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem
+                    Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
+                    this the first true generator on the Internet. It uses a dictionary of over 200 Latin words,
+                    combined with a handful of model sentence structures, to generate Lorem |psum which
+                    looks reasonable. The generated Lorem Ipsum is therefore always free from repetition,
+                    injected humour, or non-characteristic words etc.
+                </p>
+
+                <h2>Política de Privacidad 2</h2>
+
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have
+                    suffered alteration in some form, by injected humour, or randomised words which don't
+                    look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need
+                    to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem
+                    Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making
+
+                </p>
 
             </div>
 
