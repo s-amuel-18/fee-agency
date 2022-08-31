15975a1fa93a0b7e1cebefb0024862d58c20283d
s-amuel-18 <63316336+s-amuel-18@users.noreply.github.com>
2022-08-31 16:52:52 -0400

    se quitó el titulo

--- policies_and_privacy.php
+++ policies_and_privacy.php
@@ -4,9 +4,7 @@
     <!-- header -->
     <section class="pt-5" id="home">
         <div class="container">
-            <div class="text-center">
-                <h1 class="h2">Precing Table</h1>
-            </div>
+
 
             <!-- aqui va la descripcion -->
             <div class="container">
